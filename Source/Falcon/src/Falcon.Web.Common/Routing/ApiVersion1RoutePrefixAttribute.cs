0b758f1d59b165ff22632a588cf2b54174a0798c
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2017-01-15 11:41:21 +0330

    Adding API Version Constraint

---
+++ Source/Falcon/src/Falcon.Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace BM.Web.Common.Routing
+{
+    public class ApiVersion1RoutePrefixAttribute : RoutePrefixAttribute
+    {
+        private const string RouteBase = "api/{apiVersion:ApiVersionConstraint(V1)}";
+        private const string PrefixRouteBase = RouteBase + "/";
+
+        public ApiVersion1RoutePrefixAttribute(string routePrefix) 
+            : base(String.IsNullOrWhiteSpace(routePrefix) ? RouteBase : PrefixRouteBase + routePrefix) 
+        {
+            
+        }
+    }
+}
