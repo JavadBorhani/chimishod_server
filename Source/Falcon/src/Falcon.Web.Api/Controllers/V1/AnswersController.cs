06da0e7926b8def4aa0787e336903efb99cdf7b6
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2017-01-18 12:06:31 +0330

    Add Answer Controller and Models

---
+++ Source/Falcon/src/Falcon.Web.Api/Controllers/V1/AnswersController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Falcon.Database;
+using Falcon.Common;
+
+namespace Falcon.Web.Api.Controllers.V1
+{
+    public class AnswersController : ApiController
+    {
+        private DBEntity db = new DBEntity();
+        private IDateTime mDateTime;
+
+        public AnswersController(IDateTime dateTime)
+        {
+            mDateTime = dateTime;
+        }
+
+        // GET: api/Answers
+        public IQueryable<Answer> GetAnswers()
+        {
+            return db.Answers;
+        }
+
+        // GET: api/Answers/5
+        [ResponseType(typeof(Answer))]
+        public async Task<IHttpActionResult> GetAnswer(int id)
+        {
+            Answer answer = await db.Answers.FindAsync(id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(answer);
+        }
+
+        // PUT: api/Answers/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutAnswer(int id, Answer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != answer.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(answer).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnswerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [Route("/Answers/Answer/{UUID}")]
+        [ResponseType(typeof(Models.Api.Answer))]
+        [HttpPost]
+        public async Task<IHttpActionResult> PostingAnswer(string UUID, [FromBody] Models.Api.Answer answer)
+        {
+            var user = db.Users.SingleOrDefault(u => u.UUID == UUID);
+            if(user != null)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var newAnswer = new Answer
+                {
+                    QuestionID = answer.QuestionID,
+                    UserID = user.ID,
+                    YesNoState = answer.YesNoState,
+                    Liked = answer.Liked,
+                    Dislike = answer.Dislike,
+                    CreatedDate = mDateTime.Now
+                };
+
+                db.Answers.Add(newAnswer);
+                
+
+                var questionToUpdate = db.Questions.Find(answer.QuestionID);
+                if(questionToUpdate != null)
+                {
+                    if(answer.YesNoState)
+                    {
+                        ++questionToUpdate.Yes_Count;
+                    }
+                    else
+                    {
+                        ++questionToUpdate.No_Count;
+                    }
+                    if(answer.Liked != null)
+                    {
+                        ++questionToUpdate.Like_Count;
+                    }
+                    else if(answer.Dislike != null)
+                    {
+                        ++questionToUpdate.Dislike_Count;
+                    }
+                    
+                }
+                await db.SaveChangesAsync();
+                return CreatedAtRoute("DefaultApi", new { id = answer.ID }, answer);
+            }
+            return NotFound();
+        }
+
+        // DELETE: api/Answers/5
+        [ResponseType(typeof(Answer))]
+        public async Task<IHttpActionResult> DeleteAnswer(int id)
+        {
+            Answer answer = await db.Answers.FindAsync(id);
+            if (answer == null)
+            {
+                return NotFound();
+            }
+
+            db.Answers.Remove(answer);
+            await db.SaveChangesAsync();
+
+            return Ok(answer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AnswerExists(int id)
+        {
+            return db.Answers.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ Source/Falcon/src/Falcon.Web.Models/Api/Answer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Falcon.Web.Models.Api
+{    
+    public class Answer
+    {
+        public int ID { get; set; }
+
+        public int UserID { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public int QuestionID { get; set; }
+
+        public Nullable<bool> Liked { get; set; }
+
+        public Nullable<bool> Dislike { get; set; }
+
+        [Required]
+        public bool YesNoState { get; set; }
+
+        public System.DateTime CreatedDate { get; set; }
+    }
+}
