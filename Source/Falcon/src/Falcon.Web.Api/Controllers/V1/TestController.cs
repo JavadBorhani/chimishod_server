686d9f5bbe69c8c8ea8ac91908bc88cabe1a9cd1
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2017-06-27 20:33:58 +0430

    trigger

---
+++ Source/Falcon/src/Falcon.Web.Api/Controllers/V1/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Falcon.Data.QueryProcessors;
+using Falcon.EFCommonContext;
+using Falcon.EFCommonContext.DbModel;
+using Falcon.Web.Api.Utilities.Base;
+using Falcon.Web.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Falcon.Web.Api.Controllers.V1
+{
+    [UnitOfWorkActionFilter]
+    public class TestController : FalconApiController
+    {
+        private readonly IDbContext mDb;
+        private readonly IUserQueryProcessor mUser;
+
+        public TestController(IDbContext Database , IUserQueryProcessor User)
+        {
+            mDb = Database;
+            mUser = User;
+        }
+
+        [Route("TestController")]
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateJob()
+        {
+            var value = await mUser.IncreaseCoin(10);
+            return Ok(value);
+        }
+    }
+}
