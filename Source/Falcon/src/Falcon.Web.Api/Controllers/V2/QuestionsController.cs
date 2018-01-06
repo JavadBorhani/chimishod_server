48e53482542c0a1ebbf8d921152ee850c471af65
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2018-01-06 18:27:40 +0330

    Question Maintenance Processor

---
+++ Source/Falcon/src/Falcon.Web.Api/Controllers/V2/QuestionsController.cs
@@ -0,0 +1,19 @@
+﻿using Falcon.Web.Api.InquiryProcessing.Public;
+using Falcon.Web.Api.MaintenanceProcessing.Public;
+using Falcon.Web.Api.Utilities.Base;
+
+namespace Falcon.Web.Api.Controllers.V2
+{
+    public class QuestionsController : FalconApiController
+    {
+
+        private readonly IQuestionsInquiryProcessor mQuestionInquiry;
+        private readonly IQuestionsMaintenanceProcessor mQuestionMaintenance;
+
+        public QuestionsController(IQuestionsInquiryProcessor QuestionInquiry, IQuestionsMaintenanceProcessor QuestionMaintenance)
+        {
+            mQuestionInquiry = QuestionInquiry;
+            mQuestionMaintenance = QuestionMaintenance;
+        }
+    }
+}
---
+++ Source/Falcon/src/Falcon.Web.Api/MaintenanceProcessing/Private/QuestionsMaintenanceProcessor.cs
@@ -0,0 +1,15 @@
+﻿using Falcon.Data.QueryProcessors;
+using Falcon.Web.Api.MaintenanceProcessing.Public;
+
+namespace Falcon.Web.Api.MaintenanceProcessing.Private
+{
+    public class QuestionsMaintenanceProcessor : IQuestionsMaintenanceProcessor
+    {
+        private readonly IQuestionsQueryProcessor mQuestionQuery;
+        public QuestionsMaintenanceProcessor(IQuestionsQueryProcessor QuestionQuery)
+        {
+            mQuestionQuery = QuestionQuery;
+        }
+
+    }
+}
