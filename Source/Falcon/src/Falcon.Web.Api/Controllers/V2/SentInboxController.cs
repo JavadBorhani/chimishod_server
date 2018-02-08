bd275bc6721efe84279fafc81a0cee4dde61c01a
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2018-02-08 20:51:44 +0330

    sent inbox

--- Source/Falcon/src/Falcon.Web.Api/Controllers/V2/SentInboxController.cs
+++ Source/Falcon/src/Falcon.Web.Api/Controllers/V2/SentInboxController.cs
@@ -1,15 +1,39 @@
-﻿using Falcon.Web.Api.InquiryProcessing.Public;
+﻿using Falcon.Common;
+using Falcon.Web.Api.InquiryProcessing.Public;
 using Falcon.Web.Api.Utilities.Base;
+using Falcon.Web.Common;
+using Falcon.Web.Models.Api.Friend;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Falcon.Web.Api.Controllers.V2
 {
     public class SentInboxController : FalconApiController
     {
         private readonly ISentInquiryProcessor mSentInquiry;
+        private readonly IDateTime mDateTime;
         
-        public SentInboxController(ISentInquiryProcessor SentInquiry)
+        public SentInboxController(ISentInquiryProcessor SentInquiry , IDateTime DateTime)
         {
             mSentInquiry = SentInquiry;
+            mDateTime = DateTime;
+        }
+
+        [ResponseType(typeof(DataWithServerTime<SQuestionWithAnswerState>))]
+        [Route("Message/Sent")]
+        [HttpPost]
+        public async Task<DataWithServerTime<SQuestionWithAnswerState>> GetSentList(int PageNumber)
+        {
+            return null;
+        }
+
+        [ResponseType(typeof(DataWithServerTime<SQuestionWithAnswerState>))]
+        [Route("Message/Inbox")]
+        [HttpPost]
+        public async Task<IHttpActionResult> GetInboxList(int PageNumber)
+        {
+            return null;
         }
 
 
