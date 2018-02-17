1a4a0e2bfdd22f5e7845a46f2c6ee2b0a0d35e4f
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2018-02-17 21:59:49 +0330

    Working On Quest System

---
+++ Source/Falcon/src/Falcon.Web.Api/JobSystem/Private/Jobs/CalculateQuestPeopleNormal.cs
@@ -0,0 +1,24 @@
+﻿using Falcon.Web.Api.JobSystem.Public;
+using Hangfire;
+
+namespace Falcon.Web.Api.JobSystem.Private.Jobs
+{
+    public class CalculateQuestPeopleNormal : JobBase
+    {
+        public override void ActivateMode()
+        {
+            mJobManager.AddOrUpdate(() => StartJob(), Cron.MinuteInterval(2));
+        }
+
+        public override string StartJob()
+        {
+            //fetch each item and recalculate per level 
+            var value = mDb.UpdateCharacterRank();
+
+            Done = true;
+            EndTransaction();
+            return "Message  : " + value;
+        }
+
+    }
+}
