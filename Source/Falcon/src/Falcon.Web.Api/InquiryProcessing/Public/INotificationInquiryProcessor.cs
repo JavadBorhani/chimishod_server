7f9e9deb1c3865a1f9f2f8dc180bdf9d2cd1a9fe
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2018-02-21 21:50:59 +0330

    nothing

--- Source/Falcon/src/Falcon.Web.Api/InquiryProcessing/Public/INotificationInquiryProcessor.cs
+++ Source/Falcon/src/Falcon.Web.Api/InquiryProcessing/Public/INotificationInquiryProcessor.cs
@@ -1,9 +1,5 @@
 ﻿// Flapp Copyright 2017-2018
 using Falcon.Web.Models.Api;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Falcon.Web.Api.InquiryProcessing.Public
