dbee63978b2e2b0baaf39bd003ede02b672a9b1c
Javad Borhani (Jeb) <JavadBorhani@yahoo.com>
2018-05-06 19:45:36 +0430

    refactoring final quest

--- Source/Falcon/src/Falcon.Web.Models/Api/Barrett/SBarrettUserScore.cs
+++ Source/Falcon/src/Falcon.Web.Models/Api/Barrett/SBarrettUserScore.cs
@@ -1,9 +1,17 @@
-﻿namespace Falcon.Web.Models.Api.Barrett
+﻿using System.Runtime.Serialization;
+
+namespace Falcon.Web.Models.Api.Barrett
 {
+    [DataContract]
     public class SBarrettUserScore
     {
+        [IgnoreDataMember]
         public int UserID { get; set; }
+
+        [DataMember]
         public int BarrettID { get; set; }
+
+        [DataMember]
         public double Score { get; set; }
     }
 }
